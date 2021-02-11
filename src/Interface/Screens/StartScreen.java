db7756671b5b7af66d91285fcea15c3f4f844e53
martin <martinmichaux2001@gmail.com>
2021-02-11 13:06:57 +0100

    sign up added

--- src/Interface/Screens/StartScreen.java
+++ src/Interface/Screens/StartScreen.java
@@ -47,7 +47,7 @@ public class StartScreen extends Application {
     private VBox menuBox = new VBox(-5);
 
     private List<Pair<String, Runnable>> menuData = Arrays.asList(
-            new Pair<String, Runnable>("Enter", () -> {
+            new Pair<String, Runnable>("Log in", () -> {
                 try {
                     counter++;
                     for(int i=0;!login&&i<dataSet.length;i++){
@@ -72,9 +72,52 @@ public class StartScreen extends Application {
                     e.printStackTrace();
                 }
             }),
+            new Pair<String, Runnable>("Sign up", () -> {
+                try {
+                    FileWriter writer;
+                    {
+                        try {
+                            writer = new FileWriter("src\\DataBase\\users.txt");
+                            PrintWriter out = new PrintWriter(writer);
 
+                            for (int i = 0; i < dataSet.length; i++) {
+                                out.println(dataSet[i][0] + " "+dataSet[i][1]);
+                            }
+                            if(user.getText().isEmpty()||user.getText().isBlank() && psw.getText().isEmpty()||psw.getText().isBlank()){
+                                left.setText("Sorry, username or password not possible");
+                            }else{
+                                if(!userAlreadyExists()){
+                                    out.println(user.getText() + " "+psw.getText());
+                                    initializeAgents();
+                                    new MainScreen();
+                                    this.stage.close();
+                                }else{
+                                    if(user.getText().isEmpty()){
+                                        left.setText("Sorry, username not possible");
+                                    }else{
+                                        left.setText("Sorry, username already used");
+                                    }
+                                }
+                            }
+                            writer.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }),
             new Pair<String, Runnable>("Exit to Desktop", Platform::exit)
     );
+
+    public boolean userAlreadyExists(){
+        for (int i = 0; i < dataSet.length; i++) {
+            if(dataSet[i][0].equals(user.getText())) return true;
+        }
+        return false;
+    }
 
     public void initializeAgents(){
         Data.setUsername(user.getText());
@@ -96,14 +139,14 @@ public class StartScreen extends Application {
         scene.setFill(Color.BLACK);
         primaryStage.setTitle("Blokus Start Screen");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
         this.stage = primaryStage;
-
 
         left = new Text("");
         left.setFont(Font.font("Verdana", FontWeight.BOLD, 10));
         left.setFill(Color.RED);
-        left.setTranslateY(40);left.setTranslateX(0);
+        left.setTranslateY(-10);left.setTranslateX(30);
         root.getChildren().add(left);
 
         //create 2 arrays, one to store the data of the user and the other to split the data between password and username
@@ -164,17 +207,17 @@ public class StartScreen extends Application {
     private void addTitle() {
         MenuTitle title = new MenuTitle("Project 2-2 Assistant");
         title.setTranslateX(WIDTH / 2. + title.getTitleWidth()/10);
-        title.setTranslateY(HEIGHT / 3. - 50);
+        title.setTranslateY(HEIGHT / 3. );
         root.getChildren().add(title);
 
         MenuTitle username = new MenuTitle("Username :");
         username.setTranslateX(WIDTH / 2. - 100);
-        username.setTranslateY(HEIGHT / 3. + 50);
+        username.setTranslateY(HEIGHT / 3. + 90);
         root.getChildren().add(username);
 
         MenuTitle password = new MenuTitle("Password :");
         password.setTranslateX(WIDTH / 2. - 100);
-        password.setTranslateY(HEIGHT / 3. + 120);
+        password.setTranslateY(HEIGHT / 3. + 164);
         root.getChildren().add(password);
     }
 
