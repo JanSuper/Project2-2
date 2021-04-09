ea097975477971e8a44d27df33e6d5380faa339e
JanSuper <j.super@student.maastrichtuniversity.com>
2021-04-09 11:27:22 +0200

    webcam
    download the com.github.sarxos:webcam-capture:0.3.12 maven library

---
+++ src/Webcam/WebcamTest.java
@@ -0,0 +1,20 @@
+package Webcam;
+
+import com.github.sarxos.webcam.Webcam;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class WebcamTest {
+
+    public static void main(String[] args){
+        Webcam cam = Webcam.getDefault();
+        cam.open();
+        try {
+            ImageIO.write(cam.getImage(), "PNG", new File("test.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
