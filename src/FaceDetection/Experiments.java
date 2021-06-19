3ed0f50a81b57735c415c7f742e85ecf40a4d2f7
Martin Michaux <m.michaux@student.maastrichtuniversity.nl>
2021-06-19 13:35:50 +0200

    need help hahaha

--- src/FaceDetection/Experiments.java
+++ src/FaceDetection/Experiments.java
@@ -3,9 +3,13 @@ package FaceDetection;
 import DataBase.Data;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritablePixelFormat;
+import javafx.stage.Stage;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
@@ -16,32 +20,13 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-public class Experiments {
+public class Experiments extends Application {
     FaceDetection faceDetection;
 
     private Imgcodecs Highgui;
 
     public Experiments(FaceDetection faceDetection){
         this.faceDetection = faceDetection;
-    }
-
-    public static void main(String[]args) {
-        Platform.startup(new Runnable() {
-            @Override
-            public void run() {
-                Experiments experiments = null;
-                try {
-                    experiments = new Experiments(new FaceDetection());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                Image image = new Image(new File(Data.getImage()).toURI().toString(),Double.MAX_VALUE,Double.MAX_VALUE,false,true);
-                Mat frame = experiments.imageToMat(image);
-
-                Image imageDetected = experiments.detectFaceFromFrame(frame);
-            }
-        });
     }
 
     public Mat imageToMat(Image image) {
@@ -85,5 +70,33 @@ public class Experiments {
         // build and return an Image created from the image encoded in the
         // buffer
         return new Image(new ByteArrayInputStream(buffer.toArray()));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FaceDetection faceDetection = new FaceDetection();
+        this.faceDetection = faceDetection;
+
+        Image image = new Image(new File(Data.getImage()).toURI().toString(),Double.MAX_VALUE,Double.MAX_VALUE,false,true);
+        Mat frame = imageToMat(image);
+
+        Image imageDetected = detectFaceFromFrame(frame);
+
+        //Creating the image view
+        ImageView imageView = new ImageView();
+        //Setting image to the image view
+        imageView.setImage(imageDetected);
+        //Setting the image view parameters
+        imageView.setX(10);
+        imageView.setY(10);
+        imageView.setFitWidth(575);
+        imageView.setPreserveRatio(true);
+        //Setting the Scene object
+        Group root = new Group(imageView);
+        Scene scene = new Scene(root, 595, 370);
+        primaryStage.setTitle("Displaying Image");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
